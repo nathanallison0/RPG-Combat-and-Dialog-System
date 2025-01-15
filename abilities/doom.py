785a14c8a2a746ded074a91cbd4f6d2c0d0ac209
Nathan Allison <nathanallison26@gmail.com>
2025-01-14 19:35:24 -0500

    Documented doom ability and deleted unused code.

--- abilities/doom.py
+++ abilities/doom.py
@@ -5,14 +5,21 @@ class Ability:
         self.active = True
     
     def use(self, combat, instance):
+        # Store the turn the ability was used on
         instance.turnUsed = combat.turn
+        # Give notification
         combat.notif(f"{combat.player.name} fortells an impending doom!")
+        # Make ability ususable
         self.active = False
         
     def turn(self, combat, instance):
+        # If two turns have passed since the ability was used...
         if instance.turnUsed + 2 == combat.turn:
+            # Give notification
             combat.notif("A terrible force descends from the heavens...")
+            # Deal damage
             combat.damageOpponent(range(13, 16), sfx="kaboom")
+            # Make ability usable again
             self.active = True
+            # Deactivate instance
             instance.active = False
-            
